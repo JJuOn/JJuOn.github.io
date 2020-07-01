9c24e1577a3be1c010ea9ccc7cc0d519ea65afaa
서주원 <jwseo001@khu.ac.kr>
2020-07-01 15:48:28 +0900

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default () =>(
+    <Layout>
+        <div style={{color:`teal`}}>
+            <h1>About JJuOn</h1>
+            <p>KyungHee Univ.(2017~)</p>
+            <p><a href="https://github.com/jjuon">GitHub</a></p>
+        </div>
+    </Layout>
+)
